a50b79f85ce374d1445530feeff1415020071b2c
AJ1732 <ejemeniboi@gmail.com>
2025-09-23 22:51:19 +0100

    setup project

---
+++ src/components/layout/footer-comp.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+import { BrandMark } from '../elements'
+</script>
+
+<template>
+  <footer className="content-grid h-12">
+    <section className="flex items-center justify-between gap-4 px-2">
+      <RouterLink to="/" className="text-sm font-outfit font-black">
+        TODO<span className="text-avocado-500">S</span>
+      </RouterLink>
+      <BrandMark />
+    </section>
+  </footer>
+</template>
